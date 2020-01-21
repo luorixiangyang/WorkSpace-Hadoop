b6538dfb8cb25915042dbd7642050460b0feb817
zhangyongliang <zhangyongliang1231@163.com>
2020-01-21 14:40:59 +0800

    优化连接参数

--- boot-netty/netty-server/src/main/java/com/yongliang/socket/handler/NettyServerHandler.java
+++ boot-netty/netty-server/src/main/java/com/yongliang/socket/handler/NettyServerHandler.java
@@ -1,6 +1,5 @@
 package com.yongliang.socket.handler;
 
-import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.date.DateUtil;
 import com.yongliang.socket.protobuf.MessageBase;
 import com.yongliang.socket.protobuf.message.HeartbeatResponsePacket;
