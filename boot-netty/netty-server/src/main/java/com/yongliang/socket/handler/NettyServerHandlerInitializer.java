085f5eef8aadc6d4032dcf103fdea5b54c7f1230
zhangyongliang <zhangyongliang1231@163.com>
2020-01-28 12:04:15 +0800

    美化代码

--- boot-netty/netty-server/src/main/java/com/yongliang/socket/handler/NettyServerHandlerInitializer.java
+++ boot-netty/netty-server/src/main/java/com/yongliang/socket/handler/NettyServerHandlerInitializer.java
@@ -3,10 +3,17 @@ package com.yongliang.socket.handler;
 import com.yongliang.socket.protobuf.MessageBase;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.codec.protobuf.ProtobufDecoder;
 import io.netty.handler.codec.protobuf.ProtobufEncoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -18,12 +25,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 
 public class NettyServerHandlerInitializer extends ChannelInitializer<Channel> {
-
     @Override
     protected void initChannel(Channel channel) throws Exception {
         channel.pipeline()
                 //空闲检测
                 .addLast(new ServerIdleStateHandler())
+                //增加日志信息
+                .addLast(new LoggingHandler(LogLevel.INFO))
                 .addLast(new ProtobufVarint32FrameDecoder())
                 .addLast(new ProtobufDecoder(MessageBase.Message.getDefaultInstance()))
                 .addLast(new ProtobufVarint32LengthFieldPrepender())
