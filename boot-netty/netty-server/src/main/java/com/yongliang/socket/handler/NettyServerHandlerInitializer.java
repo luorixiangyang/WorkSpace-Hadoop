c916ab436276224e0804e4dd2da7acda9221a6d6
zhangyongliang <zhangyongliang1231@163.com>
2020-01-28 15:48:25 +0800

    优化完成客户端服务端异常中断问题

--- boot-netty/netty-server/src/main/java/com/yongliang/socket/handler/NettyServerHandlerInitializer.java
+++ boot-netty/netty-server/src/main/java/com/yongliang/socket/handler/NettyServerHandlerInitializer.java
@@ -3,17 +3,12 @@ package com.yongliang.socket.handler;
 import com.yongliang.socket.protobuf.MessageBase;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.group.ChannelGroup;
-import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpServerCodec;
-import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.codec.protobuf.ProtobufDecoder;
 import io.netty.handler.codec.protobuf.ProtobufEncoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
-import io.netty.handler.stream.ChunkedWriteHandler;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -23,7 +18,6 @@ import lombok.extern.slf4j.Slf4j;
  * @create 2020-01-17 15:44
  **/
 @Slf4j
-
 public class NettyServerHandlerInitializer extends ChannelInitializer<Channel> {
     @Override
     protected void initChannel(Channel channel) throws Exception {
@@ -36,6 +30,7 @@ public class NettyServerHandlerInitializer extends ChannelInitializer<Channel> {
                 .addLast(new ProtobufDecoder(MessageBase.Message.getDefaultInstance()))
                 .addLast(new ProtobufVarint32LengthFieldPrepender())
                 .addLast(new ProtobufEncoder())
+                //IP策略过滤
                 .addLast(new NettyServerHandler());
     }
 }
